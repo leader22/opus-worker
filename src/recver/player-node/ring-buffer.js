d43c741f628d602a5cca14d5040046180b3d5ce8
Yuji Sugiura <y.sugiura.0316@gmail.com>
2019-12-13 08:23:03 +0900

    Fix ring buf

--- src/recver/player-node/ring-buffer.js
+++ src/recver/player-node/ring-buffer.js
@@ -5,19 +5,31 @@ export class RingBuffer {
     this._readPos = 0;
   }
 
+  capacity() {
+    return this._buffer.length;
+  }
+
+  size() {
+    return this._writePos - this._readPos;
+  }
+
+  available() {
+    return this._buffer.length - (this._writePos - this._readPos);
+  }
+
   write(data) {
-    if (data.length > this._buffer.length) {
+    if (data.length > this._buffer.length)
       throw new Error("data size should be smaller than buffer size!");
-    }
 
     const totalSize = Math.min(data.length, this.available());
     if (totalSize == 0) return 0;
 
+    // write from current pos
     const pos = this._writePos % this._buffer.length;
     const size = Math.min(totalSize, this._buffer.length - pos);
     this._buffer.set(data.subarray(0, size), pos);
 
-    // restart from index: 0
+    // reached the last, restart from index: 0
     if (size < totalSize) {
       this._buffer.set(data.subarray(size, totalSize), 0);
     }
@@ -30,11 +42,12 @@ export class RingBuffer {
     const totalSize = Math.min(output.length, this.size());
     if (totalSize == 0) return 0;
 
+    // read from current pos
     const pos = this._readPos % this._buffer.length;
     const size = Math.min(totalSize, this._buffer.length - pos);
     output.set(this._buffer.subarray(pos, pos + size), 0);
 
-    // restart from index: 0
+    // reached the last, restart from index: 0
     if (size < totalSize) {
       output.set(this._buffer.subarray(0, totalSize - size), size);
     }
@@ -46,17 +59,5 @@ export class RingBuffer {
   clear() {
     this._writePos = 0;
     this._readPos = 0;
-  }
-
-  capacity() {
-    return this._buffer.length;
-  }
-
-  size() {
-    return this._writePos - this._readPos;
-  }
-
-  available() {
-    return this.capacity() - this.size();
   }
 }
